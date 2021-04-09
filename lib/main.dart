aa035d47db134525c02233e03535c7b2a005dac6
jindaldhruva <78293298+jindaldhruva@users.noreply.github.com>
2021-04-09 20:31:42 +0530

    color changed to red

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //new comment
       theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.blueAccent,
+          primaryColor: Colors.redAccent,//change to blue 
           scaffoldBackgroundColor: Colors.white),
       home: PriceScreen(),
     );
